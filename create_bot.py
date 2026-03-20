c522f8c250d26a6f3afcb910147d2846a48232b7
Lirangx <68253639+Alexander-ha@users.noreply.github.com>
2026-03-20 16:31:03 +0300

    Add proxy support for bot connections

--- create_bot.py
+++ create_bot.py
@@ -5,6 +5,8 @@ from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from decouple import config
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from aiohttp_socks import ProxyConnector
+
 
 scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
 admins = [int(admin_id) for admin_id in config('ADMINS').split(',')]
@@ -12,6 +14,10 @@ admins = [int(admin_id) for admin_id in config('ADMINS').split(',')]
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-bot = Bot(token=config('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+PROXY_URL = config('PROXY_URL')
+connector = ProxyConnector.from_url(PROXY_URL)
+
+session = ClientSession(connector=connector)
+bot = Bot(token=config('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML), session=session)
 
 dp = Dispatcher(storage=MemoryStorage())
